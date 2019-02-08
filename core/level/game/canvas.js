eef37736f013a4b515cfbcbc7145d677d0df7770
Yuuki Wesp <ls-micro@ya.ru>
2019-02-08 12:55:33 +0300

    add light pulse

--- core/level/game/canvas.js
+++ core/level/game/canvas.js
@@ -1,9 +1,21 @@
-$("Canvas>BlockMenu>Reset>[Button]").onClick.AddListener(function() {
-    game.control.loadLevel(2);
-});
-$("Canvas>BlockMenu>Exit>[Button]").onClick.AddListener(function() {
-    game.control.loadLevel(1);
-});
-$("Canvas>BlockMenu>Save>[Button]").interactable = false;
+go.onAwake = function(){
+    $("Canvas>BlockMenu>Reset>[Button]").onClick.AddListener(function() {
+        game.control.loadLevel(2);
+    });
+    $("Canvas>BlockMenu>Exit>[Button]").onClick.AddListener(function() {
+        game.control.loadLevel(1);
+    });
+    $("Canvas>BlockMenu>Save>[Button]").interactable = false;
+
+    
+    if(light.IsAvailable())
+    {
+        light.GetGlobal().intensity = 250;
+
+        game.animation.SmoothChange(250, 0.5, 3, function(x){
+            light.GetGlobal().intensity = x;
+        })
+    }
+}
 
 
